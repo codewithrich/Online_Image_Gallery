016405988a60c05750981b4bdb735954bcf02af8
codewithrich <richardson12230@gmail.com>
2024-09-03 14:32:17 +0530

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SendIcon from "@mui/icons-material/Send";
+import { Box, MenuItem, TextField, Button } from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { styled } from "@mui/material/styles";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+const gender = [
+  {
+    value: "male",
+    label: "M",
+  },
+  {
+    value: "female",
+    label: "F",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Image Gallery</h1>
+        <div className="Form-div">
+          <Box
+            component="form"
+            sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
+            noValidate
+            autoComplete="on"
+          >
+            <TextField
+              id="standard-basic"
+              label="Name"
+              variant="standard"
+              helperText="Please Enter your Name"
+              margin="normal"
+            />
+            <TextField
+              id="standard-basic"
+              label="Email"
+              variant="standard"
+              helperText="Please Enter your Email"
+              margin="normal"
+            />
+            <TextField
+              id="outlined-select-currency"
+              select
+              label="Select Gender"
+              variant="standard"
+              helperText="Please select your Gender"
+              margin="normal"
+            >
+              {gender.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+
+            <Button
+              sx={{ marginLeft: "20px" }}
+              component="label"
+              role={undefined}
+              variant="contained"
+              tabIndex={-1}
+              startIcon={<CloudUploadIcon />}
+            >
+              Upload files
+              <VisuallyHiddenInput
+                type="file"
+                onChange={(event) => console.log(event.target.files)}
+                multiple
+              />
+            </Button>
+
+            <Button
+              sx={{ marginLeft: "20px" }}
+              variant="contained"
+              endIcon={<SendIcon />}
+            >
+              Send
+            </Button>
+          </Box>
+        </div>
+      </div>
+    </>
   );
 }
 
